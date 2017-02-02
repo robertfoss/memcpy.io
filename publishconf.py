79b377dbc5cf25cbe92cc2367cf23ad6bef12ef9
Robert Foss <robert.foss@collabora.com>
2017-02-02 17:16:57 -0500

    Move config to all publication type

--- publishconf.py
+++ publishconf.py
@@ -13,8 +13,6 @@ from pelicanconf import *
 SITEURL = 'http://memcpy.io'
 RELATIVE_URLS = True
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
